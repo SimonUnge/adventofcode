a4ed10cc6ea16612dc34146b9736cfc68e170de0
Simon Unge <sunge@cisco.com>
2016-12-15 19:03:32 +0100

    Solved part 2. Super ugly, slow, but working

--- day5/src/day5.erl
+++ day5/src/day5.erl
@@ -12,12 +12,11 @@ get_code(DoorID) ->
     get_code(DoorID, 0, []).
 
 get_code2(DoorID) ->
-    get_code(DoorID, 0, []).
+    get_code2(DoorID, 0, []).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
-
 get_code(_DoorID, _Index, Code) when length(Code) == 8 ->
     lists:reverse(Code);
 get_code(DoorID, Index, Code) ->
@@ -31,6 +30,36 @@ get_code(DoorID, Index, Code) ->
                       Code
               end,
     get_code(DoorID, Index+1, NewCode).
+
+get_code2(_DoorID, _Index, CodeWithPos) 
+  when length(CodeWithPos) == 8 ->
+    [Code || {_, Code} <- lists:sort(CodeWithPos)];
+get_code2(DoorID, Index, CodeWithPos) ->
+    {Zeros, Rest} =
+        lists:split(5,
+                    str_to_md5_hex(DoorID, Index)),
+    NewCodeWithPos = 
+        if 
+            "00000" == Zeros ->
+                Pos = hd(Rest),
+                if 
+                    $0 =< Pos andalso Pos =< $7 ->
+                        Code = hd(tl(Rest)),
+                        case proplists:is_defined(Pos,
+                                                  CodeWithPos) 
+                        of 
+                            true ->
+                                CodeWithPos;
+                            false ->
+                                [{Pos, Code}|CodeWithPos]
+                        end;
+                    true ->
+                        CodeWithPos
+                end;
+            true ->
+                CodeWithPos
+        end,
+    get_code2(DoorID, Index+1, NewCodeWithPos).
 
 str_to_md5_hex(DoorID, Index) ->
     [begin 
